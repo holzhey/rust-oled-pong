8913f97e725d140f71a0653f2702e57eaafecbc3
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-03-09 12:15:16 +0100

    Init OLED with I2C

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
 #![no_std]
 #![no_main]
 
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
+    text::{Baseline, Text},
+};
 use panic_halt as _;
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    ufmt::uwriteln!(&mut serial, "Arduino ready!\n").unwrap();
 
     let mut led = pins.d13.into_output();
+
+    ufmt::uwriteln!(&mut serial, "Init I2C\n").unwrap();
+    let i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(),
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+
+    ufmt::uwriteln!(&mut serial, "Init OLED\n").unwrap();
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+
+    Rectangle::new(Point::new(0, 0), Size::new(127, 63))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+
+    display.set_pixel(3, 3, true);
+
+    ufmt::uwriteln!(&mut serial, "Flush text\n").unwrap();
+    display.flush().unwrap();
 
     loop {
         led.toggle();
